b0a44b2fa01f15e9c1757b37d2bfd347a80fb6dc
niraj <niraj@mapmyindia.com>
2024-11-18 18:30:29 +0530

    github page site added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://your-domain.com',
+  site: 'https://nirajsah17.github.io/fantastic-waffle_landing/',
   integrations: [
     tailwind(),
     sitemap()
